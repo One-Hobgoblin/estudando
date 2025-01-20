6dc0a852660cb1616199dc181c835b7695c02460
One-Hobgoblin <semolini2002@gmail.com>
2025-01-20 19:07:21 -0300

    aaaa

--- testes.js
+++ testes.js
@@ -6,18 +6,3 @@ console.log(x);
 
 x = aumentax(x);
 console.log(x);
-
-var nome = "murilo"
-var setNome = function(){
-    return "Guilherme";
-}
-
-console.log(nome);
-
-var novoNome = setNome();
-console.log(novoNome)
-
-var terceiroNome = nome +" " + novoNome;
-console.log(terceiroNome);
-
-//AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
